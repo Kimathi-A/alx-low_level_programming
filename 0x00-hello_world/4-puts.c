dea3f6b0b3e34f33bdb51b662701d8d50129705f
Kimathi-A <kimathiasnath@gmail.com>
2022-06-29 16:23:11 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+	/**
+	* Description: main - Prints out a string to stdout.
+	* main - Entry point
+	*
+	
 
-/**
+	
 
- * Description: main - Prints out a string to stdout.
+	
 
- * Return: 0 if success.
+	 * Return: 0 if success
+	 */
+	int main(void)
+	{
+		
 
- */
+		
 
-int main(void)
-
-{
-
-printf("with proper grammar, but the outcome is a piece of art,\n");
-
-return (0);
-
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+	}
