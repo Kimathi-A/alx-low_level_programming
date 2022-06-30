f83eca2238c79d723a3e6395a9887c620e9e0fb2
Kimathi-A <kimathiasnath@gmail.com>
2022-06-30 09:12:02 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,11 @@
 #include <stdio.h>
-
-
-
-/**
-
- * main - Prints "Programming is like building a multilingual
-
- *                puzzle, followed by a new line.
-
- *
-
- * Return: Always 0.
-
- */
-
-int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-
-
-	return (0);
-
-}
+	/**
+	* main - Prints "Programming is like building a multilingual
+	*
+	 * Return: Always 0.
+	*/
+	int main(void) /* main function */
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+	}
