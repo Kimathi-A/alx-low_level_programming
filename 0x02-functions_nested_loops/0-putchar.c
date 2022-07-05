5fa983b8cf0c8cb05fbcf38840849a54812a5e74
Kimathi-A <kimathiasnath@gmail.com>
2022-07-05 10:39:06 -0700

    readme

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,1 @@
-#include <main.h>
-/**
-*main -Entry point
-*
-*Description:'the program wrrites char'
-*
-*Return: On success 1.
-*On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
