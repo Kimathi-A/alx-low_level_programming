5154cf84b0256a435ec9a2611312e347b8af682e
Kimathi-A <kimathiasnath@gmail.com>
2022-07-05 13:51:51 -0700

    12

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Description: 'computes and prints the sum of all the multiples'
+*Return: Nothing.
+*/
+
+int main(void)
+{
+	int i, res;
+
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			res += i;
+	printf("%d\n", res);
+	return (0);
+}
