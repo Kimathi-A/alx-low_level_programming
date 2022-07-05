6755a288515475b5dd3e17eed1f62ec4f2bb547e
Kimathi-A <kimathiasnath@gmail.com>
2022-07-05 10:59:16 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - Entry point
+*
+*Description: 'the program alphapet'
+*Return: Nothing.
+*/
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+}
+}	}
+	_putchar('\n');
+}
