edb6a840274242f094c25811d34cb450d8791ead
Kimathi-A <kimathiasnath@gmail.com>
2022-07-05 14:15:16 -0700

    9

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,17 +1,80 @@
 #include <stdio.h>
 /**
 *main - Entry point
-*Description:'computes and prints the sum of all the multiples'
+*Description:'program 100'
 *Return: Nothing.
 */
 
-int main(void)
-{
-	int i, res;
 
-	for (i = 0; i < 1024; i++)
-		if (i % 3 == 0 || i % 5 == 0)
-			res += i;
-	printf("%d\n", res);
-	return (0);
+void print_times_table(int n)
+{
+
+	int num, mult, prod;
+
+
+
+	if (n >= 0 && n <= 15)
+
+	{
+
+		for (num = 0; num <= n; num++)
+
+		{
+
+			_putchar('0');
+
+
+
+			for (mult = 1; mult <= n; mult++)
+
+			{
+
+				_putchar(',');
+
+				_putchar(' ');
+
+
+
+				prod = num * mult;
+
+
+
+				if (prod <= 99)
+
+					_putchar(' ');
+
+				if (prod <= 9)
+
+					_putchar(' ');
+
+
+
+				if (prod >= 100)
+
+				{
+
+					_putchar((prod / 100) + '0');
+
+					_putchar(((prod / 10)) % 10 + '0');
+
+				}
+
+				else if (prod <= 99 && prod >= 10)
+
+				{
+
+					_putchar((prod / 10) + '0');
+
+				}
+
+				_putchar((prod % 10) + '0');
+
+			}
+
+			_putchar('\n');
+
+		}
+
+	}
+
 }
