e778ea0a12552666e431460d1b792927f54baab6
Kimathi-A <kimathiasnath@gmail.com>
2022-07-05 16:24:16 -0700

    oo

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,45 @@
+#include "main.h
+/**
+*main - entry point
+*Descrption:'ths'
+ * Return: 0
+
+ */
+
+
+
+int main(void)
+
+{
+
+int month;
+
+int day;
+
+int year;
+
+
+
+month = 2;
+
+day = 29;
+
+year = 2000;
+
+
+
+printf("Date: %02d/%02d/%04d\n", month, day , year);
+
+
+
+day = convert_day(month, day);
+
+
+
+print_remaining_days(month, day, year);
+
+
+
+return (0);
+
+}
