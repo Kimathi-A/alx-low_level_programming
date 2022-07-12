675a04926f37a130536bdc5e3cb320298b3639b7
Kimathi-A <kimathiasnath@gmail.com>
2022-07-12 03:18:37 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
+
+
 /**
-*main - Entry point
-*
-*Description:'0-reset_to_98.c program'
-*Return: void that means is correct
-*/
+
+ *reset_to_98 - updates a integer value to 98
+
+ *@n: A pointer to an int that will be updated
+
+ *
+
+ *Return: void that means is correct
+
+ */
+
+
+
 void reset_to_98(int *n)
+
+
+
 {
-	*n = 98;
+
+*n = 98;
+
 }
