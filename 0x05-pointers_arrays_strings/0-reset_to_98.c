2b5e0a3ae42ca273b877fef4e68d0b71a471c179
Kimathi-A <kimathiasnath@gmail.com>
2022-07-12 03:53:43 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
-*main - Entry point
-*Description: '98 degrees'
-*
+*main -Entry point
+*@n: argument to the function.
+*Description: print out the required result.
 *Return: return void.
 */
 void reset_to_98(int *n)
