0e2a8bd5d1f2a4e3b0c38506328b9fba091d70c6
Kimathi-A <kimathiasnath@gmail.com>
2022-07-12 13:27:39 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,22 +4,14 @@
 
 /**
 
- *reset_to_98 - updates a integer value to 98
+ *main - Entry point 
 
- *@n: A pointer to an int that will be updated
-
+ *Description: 'updates a integer value to 98'
  *
-
  *Return: void that means is correct
-
  */
 
-
-
 void reset_to_98(int *n)
-
-
-
 {
 
 *n = 98;
