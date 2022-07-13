9479e4c73b3280166e8dc5127764ab6fba1956df
Kimathi-A <kimathiasnath@gmail.com>
2022-07-13 13:26:02 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,107 +1,93 @@
+
+
 #include "main.h"
-
 #include <stdio.h>
-
 /**
 
  * infinite_add - adds two numbers
 
- * @n1: number one.
+ * @n1: first number
 
- * @n2: number two.
+ * @n2: second number
 
- * @r: buffer that the function will use to store the result.
+ * @r: buffer for result
 
- * @size_r: buffer size:
+ * @size_r: buffer size
 
- * Return: the pointer to dest.
+ *
+
+ * Return: address of r or 0
 
  */
-
-
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 {
 
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int i, j, k, l, m, n;
 
 
 
-	while (*(n1 + c1) != '\0')
+	for (i = 0; n1[i]; i++)
 
-		c1++;
+		;
 
-	while (*(n2 + c2) != '\0')
+	for (j = 0; n2[j]; j++)
 
-		c2++;
+		;
 
-	if (c1 >= c2)
-
-		bg = c1;
-
-	else
-
-		bg = c2;
-
-	if (size_r <= bg + 1)
+	if (i > size_r || j > size_r)
 
 		return (0);
 
-	r[bg + 1] = '\0';
+	m = 0;
 
-	c1--, c2--, size_r--;
-
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-
-	while (bg >= 0)
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 
 	{
 
-		op = dr1 + dr2 + add;
+		n = m;
 
-		if (op >= 10)
+		if (i >= 0)
 
-			add = op / 10;
+			n += n1[i] - '0';
 
-		else
+		if (j >= 0)
 
-			add = 0;
+			n += n2[j] - '0';
 
-		if (op > 0)
+		if (i < 0 && j < 0 && n == 0)
 
-		*(r + bg) = (op % 10) + 48;
+		{
 
-		else
+			break;
 
-			*(r + bg) = '0';
+		}
 
-		if (c1 > 0)
+		m = n / 10;
 
-			c1--, dr1 = *(n1 + c1) - 48;
-
-		else
-
-			dr1 = 0;
-
-		if (c2 > 0)
-
-			c2--, dr2 = *(n2 + c2) - 48;
-
-		else
-
-			dr2 = 0;
-
-		bg--, size_r--;
+		r[k] = n % 10 + '0';
 
 	}
 
-	if (*(r) == '0')
+	r[k] = '\0';
 
-		return (r + 1);
+	if (i >= 0 || j >= 0 || m)
 
-	else
+		return (0);
 
-		return (r);
+	for (k -= 1, l = 0; l < k; k--, l++)
+
+	{
+
+		m = r[k];
+
+		r[k] = r[l];
+
+		r[l] = m;
+
+	}
+
+	return (r);
 
 }
