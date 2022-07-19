b80cccc70d73f702823cbbd8ec843a6196a0d790
Kimathi-A <kimathiasnath@gmail.com>
2022-07-19 09:17:34 -0700

    Task 5

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,53 +1,28 @@
 #include<string.h>
-
-
+#include "main.h"
 
 /**
-
- * _strpbrk - searches a string for any of a set of bytes
-
- * @s: pointer to string to scan
-
- * @accept: character to search the string for
-
- * Return: first character
-
- */
-
-
+* _strpbrk - searches a string for any of a set of bytes
+* @s: pointer to string to scan
+* @accept: character to search the string for
+* Description:'the prm'
+* Return: first character
+*/
 
 char *_strpbrk(char *s, char *accept)
-
 {
-
 	int i, j;
 
-
-
 	if ((s == NULL) && (accept == NULL))
-
 		return (NULL);
 
-
-
 	for (i = 0; s[i] != '\0'; s++)
-
 	{
-
 		for (j = 0; accept[j] != '\0'; j++)
-
 		{
-
 			if (s[i] == accept[j])
-
 				return (s);
-
 		}
-
 	}
-
-
-
 	return (NULL);
-
 }
